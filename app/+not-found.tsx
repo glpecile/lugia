9447c44e81a192b514e3325f0e1eaff1d84228e3
Gian <58370608+glpecile@users.noreply.github.com>
2024-11-27 18:54:13 -0300

    style: tweak not found

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,17 +1,21 @@
 import { Link, Stack } from "expo-router";
 import { Fragment } from "react";
 import { View } from "react-native";
-import { Text } from "~/components/ui/text";
+import { H1, P } from "~/components/ui/typography";
 
 export default function NotFoundScreen() {
     return (
         <Fragment>
             <Stack.Screen options={{ title: "Oops!" }} />
-            <View>
-                <Text>This screen doesn't exist.</Text>
+            <View className="flex-1 justify-start items-center gap-5 p-3.5 bg-secondary/30">
+                <H1>404</H1>
+
+                <P>This screen doesn't exist.</P>
 
                 <Link href="/">
-                    <Text>Go to home screen!</Text>
+                    <P className="underline underline-offset-2">
+                        Go to home screen
+                    </P>
                 </Link>
             </View>
         </Fragment>

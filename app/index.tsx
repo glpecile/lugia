0ac7119cc0e24edfebc6bd3971d84c7b8b08a12c
Gian <58370608+glpecile@users.noreply.github.com>
2024-11-27 18:05:28 -0300

    style: edit blog post card

--- app/index.tsx
+++ app/index.tsx
@@ -13,27 +13,23 @@ export default function Screen() {
     const posts = getBlogPosts();
 
     return (
-        <View className="flex-1 justify-start items-center gap-5 p-3 bg-secondary/30">
+        <View className="flex-1 justify-start items-center gap-5 p-3.5 bg-secondary/30">
             {posts.map((item) => (
-                <View key={item.date} className="py-4">
-                    {
-                        <Link
-                            // @ts-expect-error: the date is always a string
-                            href={`${item.href}`}
-                            key={item.title}
-                            className="w-full"
-                        >
-                            <Card className="w-fit">
-                                <CardHeader>
-                                    <CardTitle>{item.title}</CardTitle>
-                                    <CardDescription>
-                                        {item.description}
-                                    </CardDescription>
-                                </CardHeader>
-                            </Card>
-                        </Link>
-                    }
-                </View>
+                <Link
+                    // @ts-expect-error: the date is always a string
+                    href={`${item.href}`}
+                    key={item.date}
+                    className="w-full flex items-center justify-center"
+                >
+                    <Card className="w-full">
+                        <CardHeader>
+                            <CardTitle>{item.title}</CardTitle>
+                            <CardDescription>
+                                {item.description}
+                            </CardDescription>
+                        </CardHeader>
+                    </Card>
+                </Link>
             ))}
         </View>
     );

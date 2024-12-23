7ff89e11fcc95841c510c2f57a5a6179acbbf64f
Gian <58370608+glpecile@users.noreply.github.com>
2024-12-22 21:45:27 -0300

    fix: add monorepo nativewind config
    https://github.com/nativewind/nativewind/issues/572#issuecomment-1752149051

--- metro.config.js
+++ metro.config.js
@@ -11,6 +11,16 @@ config.resolver.assetExts = config.resolver.assetExts.filter(
 
 config.transformer.babelTransformerPath = require.resolve("./transformer.js");
 
+// This is not needed for NativeWind, it is configuration for Metro to understand monorepos
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, "../..");
+config.watchFolders = [workspaceRoot];
+config.resolver.disableHierarchicalLookup = true;
+config.resolver.nodeModulesPaths = [
+    path.resolve(projectRoot, "node_modules"),
+    path.resolve(workspaceRoot, "node_modules"),
+];
+
 const { withNativeWind } = require("nativewind/metro");
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+module.exports = withNativeWind(config, { input: "./global.css", projectRoot });
